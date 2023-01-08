1c135c0c9d38a30208d644e6fdb321353bff6f3a
Your Name <you@example.com>
2023-01-07 17:03:52 -0800

    promises exercise

---
+++ part_1/promise1.js
@@ -0,0 +1,43 @@
+// $.getJSON('http://numbersapi.com/7?json', response => {
+//     let res = response            
+// console.log(res);
+//     $('#number').text(res.text);
+
+    // {text: '7 is the number of seconds it takes "Superman: Esc…oller coaster to go from 0 to 100 miles per hour.', number: 7, found: true, type: 'trivia'}
+
+// });
+
+
+
+// let favNumbers = [7, 11, 22];
+// $.getJSON(`http://numbersapi.com/${favNumbers}?json`).then(data => {
+//   console.log(data);
+// });
+
+
+
+
+
+// let number = 7;
+// facts = [];
+
+// for (let i=1; i<5; i++){
+//     facts.push(axios.get(`http://numbersapi.com/${number}?json`))
+// }
+
+// Promise.all(facts)
+//     .then(factsArr => {
+//         // for (res of factsArr){
+//         //     $("body").append(`<p>${res.data.text}</p>`)
+//         // }
+
+//         // Using forEach
+//         factsArr.forEach(res => {$("body").append(`<p>${res.data.text}</p>`)
+            
+//         });
+
+//     })
+//     .catch(err => console.log(err));
+
+
+
